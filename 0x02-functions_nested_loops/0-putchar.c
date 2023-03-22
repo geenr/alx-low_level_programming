9bb677d4b22a5fb758790e85060d0602f555a696
eugenekimani <ekiukkim90@gmail.com>
2023-03-21 17:15:13 -0700

    0putcher

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,9 +4,9 @@
  * main - Entry point
  * Return: Always 0
  */
-
 int main(void)
 {
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -15,5 +15,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
