1552dca5558822a577b5b69e82fcdde0e5fa60ba
eugenekimani <ekiukkim90@gmail.com>
2023-04-28 15:41:24 -0700

    stringss

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,9 @@
-section .data
-    hello db "Hello, Holberton", 0Ah, 00h  ; string to print, with newline and null terminator
-
-section.text; Code section.
-
-global main; standard gcc point
-
-main:; program label for entry point
-push rbp ; set up stack frame, must allign
-
-mov rdi, fmt
-mov rsi,msg
-mov rax, 0; or can be xor rax, rax
-call printf; Call C function
-
-pop rbp;restore stack frame
-
-mov rax 0; normal, no error, return value
-ret; return
+global    main; stahdard gcc entry point
+          extern    printf
+main:; the program label for entry point
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
