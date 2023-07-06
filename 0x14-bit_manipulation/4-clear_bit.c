f0e3f1471935ba66cada7dad7f4491bc519a5123
eugenekimani <ekiukkim90@gmail.com>
2023-07-06 16:41:23 -0700

    bit manipularion

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,10 +4,13 @@
  * clear_bit - sets the value of a bit to 0 at a given index
  * @n: pointer to the number being represented as binary
  * @index: the index starting from 0 of the bit you want to set
- * Returns: 1 if Success, or -1 if Failure
+ * Return: 1 for Success, -1 for Failure
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	if (index > 63)
+		return (-1);
+	*n = (~(1UL << index) & *n);
 	return (1);
 }
