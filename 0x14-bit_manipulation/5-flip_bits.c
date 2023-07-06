30210ea93ea2583bd932ec0e1618a7bd91652378
eugenekimani <ekiukkim90@gmail.com>
2023-07-06 16:56:54 -0700

    bit manipularion

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,12 +1,24 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits you would need to flip to get from one number to another
+ * flip_bits - returns number of bits needed to flip
+ * to get from one number to another
  * @n: the number to be changed
  * @m: the new number formed after the flip
+ * Return: number of bits to be flipped
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	return (0);
+	int val, county = 0;
+	unsigned int currentNo;
+	unsigned long int exclve = n ^ m;
+
+	for (val = 63; val >= 0; val--)
+	{
+		currentNo = exclve >> val;
+		if (currentNo & 1)
+			county++;
+	}
+	return (county);
 }
