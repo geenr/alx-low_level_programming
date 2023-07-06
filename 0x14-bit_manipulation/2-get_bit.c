4f1294dd30dc07e350d221ccb31be4c16416da98
eugenekimani <ekiukkim90@gmail.com>
2023-07-06 16:09:58 -0700

    bit manipularion

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,5 +10,10 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return -1;
+	int valueOfBit;
+
+	if (index > 63)
+		return (-1);
+	valueOfBit = (n >> index) & 1;
+	return (valueOfBit);
 }
