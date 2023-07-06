30396fe83d33a3541a1223b8672d65b3ea640cb6
eugenekimani <ekiukkim90@gmail.com>
2023-07-06 16:02:54 -0700

    bit manipularion

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: the number that is represented by the binary number
+ * @index: the index, starting from 0 of the bit you want to get
+ *
+ * Return: the value of the bit at index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return -1;
+}
