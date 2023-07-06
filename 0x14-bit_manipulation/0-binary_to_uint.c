1efc5b6a1460ebee863096c95b912a84584905f2
eugenekimani <ekiukkim90@gmail.com>
2023-07-06 14:40:16 -0700

    bitmanipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+ * binary_to_uint - converts a binary number to an unsigned integer or decimal format
+ * @b: string which contains the binary number
+ *
+ * return: the converted number/ the decimal format
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	return ();
+}
