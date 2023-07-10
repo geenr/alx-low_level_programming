7a5cdf1a03695455a87820cef9d23e461ef5d7be
eugenekimani <ekiukkim90@gmail.com>
2023-07-10 13:25:14 -0700

    fileio

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * read_textfile -  function that read text file
+ * and prints to POSIX standard output
+ * @letters: number of letters to be read and printed
+ * @filename: textfile to be read
+ *
+ * Return: 0 when function fail or filename is NULL
+ * 	w- Actual Number of Bytes to be read and printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+file I/O
+Write a function that reads a text file and prints it to the POSIX standard output. - oflag used O_RDONLY
