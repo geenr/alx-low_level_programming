0da4ecfd538bd1f30703a30496a4baf21d004f99
eugenekimani <ekiukkim90@gmail.com>
2023-07-10 14:23:58 -0700

    fileio

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * create_file - function that creates a file
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - function that appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end of the file
+ *
+ * Return: 1 for success, -1 for failure or filename is NULL
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+}
