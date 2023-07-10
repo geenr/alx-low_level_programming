e4e7f00150240e57cdbabb8e25d104337f4db4e2
eugenekimani <ekiukkim90@gmail.com>
2023-07-10 13:37:21 -0700

    fileio

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,10 +7,24 @@
  * @filename: textfile to be read
  *
  * Return: 0 when function fail or filename is NULL
- * 	w- Actual Number of Bytes to be read and printed
+ * w- Actual Number of Bytes to be read and printed
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *buffy;
+	ssize_t fp;
+	ssize_t wter;
+	ssize_t rder;
 
+	fp = open(filename, O_RDONLY);
+	if (fp == -1)
+		return (0);
+	buffy = malloc(sizeof(char) * letters);
+	rder = read(fp, buffy, letters);
+	wter = write(STDOUT_FILENO, buffy, rder);
+
+	free(buffy);
+	close(fp);
+	return (wter);
 }
