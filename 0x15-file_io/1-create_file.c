20d5ecc4aa66f42cd88cdac6b5d994661b452dac
eugenekimani <ekiukkim90@gmail.com>
2023-07-10 15:17:23 -0700

    fileio

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		for (lenz = 0; text_content[lenz];)
 			lenz++;
 	}
-	fp = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wter = write(fp, text_content, lenz);
 
 	if (fp == -1 || wter == -1)
