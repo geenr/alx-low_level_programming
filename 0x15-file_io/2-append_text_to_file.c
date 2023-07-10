bd072a5cdbbe24163958a55a1a10d66e5635f849
eugenekimani <ekiukkim90@gmail.com>
2023-07-10 14:33:10 -0700

    fileio

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,4 +10,23 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int op, wter, lenz = 0;
+
+	if (filename == 0)
+		return (-1);
+	if (text_content != 0)
+	{
+		for (lenz = 0; text_content[lenz];)
+			lenz++;
+	}
+
+	op = open(filename, O_WRONLY | O_APPEND);
+	wter = write(op, text_content, lenz);
+
+	if (op == -1 || wter == -1)
+		return (-1);
+
+	close(op);
+
+	return (1);
 }
